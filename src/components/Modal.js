8de65471e5b9bb0b37d10a671448928c4e007da0
Daniel <daniel-mcintyre@hotmail.com>
2022-08-25 13:13:47 -0400

    updates to cmodal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,8 +2,13 @@
 export const Modal = ({showModal, setShowModal}) => {
     return (
         <>
-            {showModal ? <div>Modal</div> : null}
-            
+            {showModal ? <div className="modal-background">
+                <div className="modal-box">
+                    Modal
+                    Content
+                </div>
+                <button className="close-modal-button">X</button>
+            </div> : null}    
         </>
     )
 }
